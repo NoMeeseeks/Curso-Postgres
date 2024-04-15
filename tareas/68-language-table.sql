b6f12aa60c7c12eb51649ce925b9401cc6d4f5a1
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-15 12:50:04 -0500

    resolucion de la tarea

--- tareas/68-language-table.sql
+++ tareas/68-language-table.sql
@@ -1,31 +1,33 @@
-
-
 -- Tarea con countryLanguage
 
 -- Crear la tabla de language
 
 -- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS language_code_seq;
-
+CREATE SEQUENCE IF NOT EXISTS LANGUAGE_CODE_SEQ;
 
 -- Table Definition
 CREATE TABLE "public"."language" (
-    "code" int4 NOT NULL DEFAULT 	nextval('language_code_seq'::regclass),
-    "name" text NOT NULL,
+    "code" INT4 NOT NULL DEFAULT NEXTVAL('language_code_seq'::REGCLASS),
+    "name" TEXT NOT NULL,
     PRIMARY KEY ("code")
 );
 
 -- Crear una columna en countrylanguage
-ALTER TABLE countrylanguage
-ADD COLUMN languagecode varchar(3);
-
+ALTER TABLE COUNTRYLANGUAGE ADD COLUMN LANGUAGECODE VARCHAR(3);
 
 -- Empezar con el select para confirmar lo que vamos a actualizar
+select language ,
+(select * code from language l where cl.language = l.name)
+from countrylanguage cl
 
 -- Actualizar todos los registros
+insert into language (name) select distinct cl.language from countrylanguage cl order by cl.language asc
+
+update countrylanguage cl set languagecode = (select code from language l where cl.language = l.name)
 
 -- Cambiar tipo de dato en countrylanguage - languagecode por int4
-
+alter table countrylanguage alter column languagecode type int4 using languagecode::INTEGER 
 -- Crear el forening key y constraints de no nulo el language_code
-
+alter table countrylanguage add constraint FK_LANGUAGE_CODE FOREIGN KEY
+	(languagecode) REFERENCES language(CODE)
 -- Revisar lo creado
