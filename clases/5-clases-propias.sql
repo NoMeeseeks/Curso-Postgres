d1f1aec031950cefc76524da016ed5e8fd9099ce
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-07-15 22:52:09 -0500

    full outer join

--- clases/5-clases-propias.sql
+++ clases/5-clases-propias.sql
@@ -1,30 +1,76 @@
 --1#-clausa-union
 --una forma de unir resultados
-select * from continent where "name" like '%America%'
-union
-select * from continent where code in (3,5)
-order by "name" asc;
+SELECT
+    *
+FROM
+    CONTINENT
+WHERE
+    "name" LIKE '%America%'
+UNION
+SELECT
+    *
+FROM
+    CONTINENT
+WHERE
+    CODE IN (3, 5)
+ORDER BY
+    "name" ASC;
+
 --nota: tienen que tener el mismo numero de columnas y el mismo orden
 
 --2#-clausula-where
 --hacer union por where
-select c.name as pais ,c2.name as continente
-from country c,continent c2  
-where c.continent  = c2.code 
-order by c2.name ;
+SELECT
+    C.NAME  AS PAIS,
+    C2.NAME AS CONTINENTE
+FROM
+    COUNTRY   C,
+    CONTINENT C2
+WHERE
+    C.CONTINENT = C2.CODE
+ORDER BY
+    C2.NAME;
+
 --hacer uniones por el where puede llegar a ser volatil en casos de no especificarlo
 
 --3#-clausula-inner-join
---solo muestra los datos que se relacionan en la tabla a y b 
-select c.name as pais ,c2.name as continente
-from country c inner join continent c2  on c.continent = c2.code 
-order by c2.name ;
+--solo muestra los datos que se relacionan en la tabla a y b
+SELECT
+    C.NAME  AS PAIS,
+    C2.NAME AS CONTINENTE
+FROM
+    COUNTRY   C
+    INNER JOIN CONTINENT C2
+    ON C.CONTINENT = C2.CODE
+ORDER BY
+    C2.NAME;
 
 --Alterar secuencias
-alter sequence 'sequenceName' restart with 8;
+ALTER SEQUENCE 'sequenceName' RESTART WITH 8;
 
-select * from continent c ;
-insert into continent values(9,'South Oceania')
+SELECT
+    *
+FROM
+    CONTINENT C;
 
---
-
+INSERT INTO CONTINENT VALUES(
+    9,
+    'South Oceania'
+)
+ --
+INSERT INTO CONTINENT VALUES(
+    11,
+    'South Oceania'
+) INSERT INTO CONTINENT VALUES(
+    12,
+    'South Oceania'
+)
+ --country a  -> name , continet(codigo numerico)
+ -- continent b  -> name as continentName
+    SELECT
+        CO.NAME AS CODIGO,
+        C.CODE
+    FROM
+        COUNTRY CO
+        FULL OUTER JOIN CONTINENT C
+        ON CO.CONTINENT = C.CODE
